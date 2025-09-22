004edc92ab4686839b2b94768f9f0d43c17d1497
nashit-tabasum <nashit@oddly.co>
2025-09-22 12:21:09 +0600

    page-dashboard

--- wp-templates/page-dashboard.tsx
+++ wp-templates/page-dashboard.tsx
@@ -157,33 +157,35 @@ const Section: React.FC<SectionProps> = ({
 }) => {
   return (
     <Link href={url}>
-      <div className="bg-white py-12 md:py-16 xl:py-20">
-        <div className="mx-auto max-w-7xl px-5 md:px-10 xl:px-16">
-          <div className="border border-slate-400 shadow-2xl rounded-lg p-3 lg:p-12 transition-all duration-300 ease-in-out hover:-translate-y-1.5">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-8 items-center">
-              {/* Left Column */}
-              <div>
-                <h2 className="font-family-montserrat text-slate-950 font-normal text-2xl md:text-3xl xl:text-4xl leading-snug">
-                  {title}
-                </h2>
-                <p className="font-family-sourcecodepro text-slate-950 font-normal text-base/6 mt-0.5 lg:mt-2">
-                  {text}
-                </p>
-                <div className="mt-4 lg:mt-5">
-                  <WhiteButton url={url}>Learn more</WhiteButton>
+      <div className="py-12 md:py-16 xl:py-20">
+        <div className="bg-white">
+          <div className="mx-auto max-w-7xl px-5 md:px-10 xl:px-16">
+            <div className="border border-slate-400 shadow-2xl rounded-lg p-3 lg:p-12 transition-all duration-300 ease-in-out hover:-translate-y-1.5">
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-8 items-center">
+                {/* Left Column */}
+                <div>
+                  <h2 className="font-family-montserrat text-slate-950 font-normal text-2xl md:text-3xl xl:text-4xl leading-snug">
+                    {title}
+                  </h2>
+                  <p className="font-family-sourcecodepro text-slate-950 font-normal text-base/6 mt-0.5 lg:mt-2">
+                    {text}
+                  </p>
+                  <div className="mt-4 lg:mt-5">
+                    <WhiteButton url={url}>Learn more</WhiteButton>
+                  </div>
                 </div>
-              </div>
 
-              {/* Right Column */}
-              <div>
-                <img
-                  src={imgSrc}
-                  alt={imgAlt}
-                  className="w-full h-full rounded-lg"
-                  width={511}
-                  height={503}
-                  loading="lazy"
-                />
+                {/* Right Column */}
+                <div>
+                  <img
+                    src={imgSrc}
+                    alt={imgAlt}
+                    className="w-full h-full rounded-lg"
+                    width={511}
+                    height={503}
+                    loading="lazy"
+                  />
+                </div>
               </div>
             </div>
           </div>
