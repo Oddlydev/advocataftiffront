1cc6c59c1a4447f0e56f859ef07a652c6ab0f73d
SheharaF01 <shehara.oddly@gmail.com>
2025-10-08 10:23:42 +0530

    add test page

---
+++ pages/cover-hero.tsx
@@ -0,0 +1,10 @@
+import HeroCoverImage from "@/src/components/HeroBlocks/HeroCoverImage";
+
+export default function CoverHero() {
+  return (
+    <>
+      <HeroCoverImage />
+      <div className="bg-white h-[500px] w-full"></div>
+    </>
+  );
+}
