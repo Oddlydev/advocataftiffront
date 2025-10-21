fda02902353710210ac6f1d62f068e1be6f283c1
nashit-tabasum <nashit@oddly.co>
2025-10-21 21:44:58 +0600

    table scroll padding bottom

--- wp-templates/page-state-owned-enterprises.tsx
+++ wp-templates/page-state-owned-enterprises.tsx
@@ -555,7 +555,7 @@ export default function PageStateOwnedDashboard(): JSX.Element {
             </p>
           )}
           {filteredPosts[0]?.methodologyFileUrl && (
-            <div className="bg-gray-50 rounded-lg px-6 py-3.5 mt-3">
+            <div className="bg-gray-50 rounded-lg px-6 py-3.5 mt-8">
               <div className="grid grid-cols-1 md:flex md:justify-between gap-4 text-xs/4 text-slate-600 font-sourcecodepro">
                 <div className="text-slate-600 text-xs/4 font-normal font-sourcecodepro flex items-center gap-2">
                   <p>
