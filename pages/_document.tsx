35a589d472466424fad82df60f7fcc631e5bce08
SheharaF01 <shehara.oddly@gmail.com>
2025-10-23 17:45:24 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      {" "}
-      {/* 👈 Add your language here */}
       <Head />
       <body>
         <Main />
