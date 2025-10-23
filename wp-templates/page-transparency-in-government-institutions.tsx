c32b26cb6f580f16df2334463c7fcfd8483edb59
SheharaF01 <shehara.oddly@gmail.com>
2025-10-23 15:31:29 +0530

    Update page-transparency-in-government-institutions.tsx

--- wp-templates/page-transparency-in-government-institutions.tsx
+++ wp-templates/page-transparency-in-government-institutions.tsx
@@ -555,6 +555,154 @@ export default function PageTransparencyDashboard(): JSX.Element {
               </div>
             </div>
           )}
+          <div className="mx-auto max-w-7xl pt-6 md:pt-9 pb-16">
+            <div>
+              <div className="grid xl:flex gap-4 xl:gap-7 items-center justify-start xl:justify-end w-full">
+                <div>
+                  <p className="text-sm xl:text-base/6 font-medium font-sourcecodepro text-slate-600">
+                    Interpretation of the indicators:
+                  </p>
+                </div>
+                <div className="grid md:flex items-center gap-2.5 md:gap-5">
+                  {/* Good */}
+                  <div className="flex items-center gap-3 md:border-r border-slate-300 pr-3 md:pr-4">
+                    <span className="text-sm/tight font-medium font-sourcecodepro text-slate-600">
+                      Good
+                    </span>
+                    <div className="flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                      >
+                        <circle cx="6" cy="6" r="6" fill="#22C55E" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  {/* Marginal Success */}
+                  <div className="flex items-center gap-3 md:border-r border-slate-300 pr-3 md:pr-4">
+                    <span className="text-sm/tight font-medium font-sourcecodepro text-slate-600">
+                      Average
+                    </span>
+                    <div className="flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                      >
+                        <circle cx="6" cy="6" r="6" fill="#F59E0B" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  {/* Poor */}
+                  <div className="flex items-center gap-3">
+                    <span className="text-sm/tight font-medium font-sourcecodepro text-slate-600">
+                      Poor
+                    </span>
+                    <div className="flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                      >
+                        <circle cx="6" cy="6" r="6" fill="#DC2626" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="grid xl:flex gap-4 xl:gap-7 mt-3 items-center justify-start xl:justify-end w-full">
+                <div>
+                  <p className="text-sm xl:text-base/6 font-medium font-sourcecodepro text-slate-600">
+                    Interpretation of composite score:
+                  </p>
+                </div>
+                <div className="grid md:flex items-center gap-2.5 md:gap-5">
+                  {/* Good */}
+                  <div className="flex items-center gap-3 md:border-r border-slate-300 pr-3 md:pr-4">
+                    <span className="text-sm/tight font-medium font-sourcecodepro text-slate-600">
+                      Successful
+                    </span>
+                    <div className="flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                      >
+                        <circle cx="6" cy="6" r="6" fill="#22C55E" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  {/* Marginal Success */}
+                  <div className="flex items-center gap-3 md:border-r border-slate-300 pr-3 md:pr-4">
+                    <span className="text-sm/tight font-medium font-sourcecodepro text-slate-600">
+                      Marginally Successful
+                    </span>
+                    <div className="flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                      >
+                        <circle cx="6" cy="6" r="6" fill="#F59E0B" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  {/* Average */}
+                  <div className="flex items-center gap-3 md:border-r border-slate-300 pr-3 md:pr-4">
+                    <span className="text-sm/tight font-medium font-sourcecodepro text-slate-600">
+                      Unsuccessful
+                    </span>
+                    <div className="flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                      >
+                        <circle cx="6" cy="6" r="6" fill="#F97316" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  {/* Poor */}
+                  <div className="flex items-center gap-3">
+                    <span className="text-sm/tight font-medium font-sourcecodepro text-slate-600">
+                      Total Failure
+                    </span>
+                    <div className="flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                      >
+                        <circle cx="6" cy="6" r="6" fill="#DC2626" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
