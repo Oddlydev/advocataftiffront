5995ee2767660adef5cfc7c3668e111b4c057a17
SheharaF01 <shehara.oddly@gmail.com>
2025-10-23 17:44:59 +0530

    lang attribute

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      {" "}
+      {/* 👈 Add your language here */}
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
