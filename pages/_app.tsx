a1a8e295b28b1a825e0726b796b806d876910bef
SheharaF01 <shehara.oddly@gmail.com>
2026-02-03 11:25:20 +0530

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 // pages/_app.tsx
 import "../faust.config";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { AppProps } from "next/app";
 import { FaustProvider } from "@faustwp/core";
+import Script from "next/script";
 import "../styles/globals.css";
 
 import HeaderNav from "@/src/components/header";
@@ -24,9 +25,44 @@ import {
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
+  useEffect(() => {
+    if (!gaId) return;
+
+    const handleRouteChange = (url: string) => {
+      const gtag = (window as { gtag?: (...args: any[]) => void }).gtag;
+      if (gtag) {
+        gtag("config", gaId, { page_path: url });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [gaId, router.events]);
 
   return (
     <FaustProvider pageProps={pageProps}>
+      {gaId ? (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+            strategy="afterInteractive"
+          />
+          <Script
+            id="ga-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaId}', { page_path: window.location.pathname });
+              `,
+            }}
+          />
+        </>
+      ) : null}
       <div
         className={`${inter.variable} ${montserrat.variable} ${manrope.variable} ${playfair.variable} ${sourceCodePro.variable} ${baskervville.variable}`}
       >
